4810367c2de76365e23e081078199aed40459a5a
Tobias Waurick <s1550973@tu-dresden.de>
2017-07-12 13:25:28 +0200

    removed unneeded files, added module header(empty)

---
+++ src/compressed-sensing/model/compressed-sensing-module.h
@@ -0,0 +1,10 @@
+/**
+ * \defgroup compsens Compressed Sensing
+ *
+ * Compressed sensing Applications, Coding/ Decoding classes
+ */
+
+#ifndef COMPRESSED_SENSING_MODULE_H
+#define COMPRESSED_SENSING_MODULE_H
+
+#endif
