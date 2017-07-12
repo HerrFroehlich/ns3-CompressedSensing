98c0bf5aa9ba57b2353f5bf3d9870ae90fba7ec0
Tobias Waurick <s1550973@tu-dresden.de>
2017-07-12 11:27:23 +0200

    Added Compressor

---
+++ src/compressed-sensing/model/compressor.cc
@@ -0,0 +1,127 @@
+/**
+* \file compressor.cc
+*
+* \author Tobias Waurick
+* \date 11.07.17
+*
+*/
+#include "compressor.h"
+#include <armadillo>
+#include "assert.h"
+
+NS_LOG_COMPONENT_DEFINE("Compressor");
+
+template <>
+TypeId Compressor<double>::GetTypeId(void)
+{
+	static TypeId tid = TypeId("ns3::Compressor<double>")
+							.SetParent<Object>()
+							.AddConstructor<Compressor<double>>()
+							.SetGroupName("CompressedSensing")
+							.AddAttribute("RanMatrix", "The underlying random matrix form to create the sensing matrix",
+										  TypeId::ATTR_SET,
+										  PointerValue(CreateObject<IdentRandomMatrix>()),
+										  MakePointerAccessor(&Compressor::SetRanMat),
+										  MakePointerChecker<RandomMatrix>())
+							.AddAttribute("TransMatrix", "The underlying matrix of a real transformation in which the solution is sparse",
+										  TypeId::ATTR_SET,
+										  PointerValue(),
+										  MakePointerAccessor(&Compressor::SetTransMat),
+										  MakePointerChecker<TransMatrix<double>>());
+	return tid;
+}
+
+template <>
+TypeId Compressor<cx_double>::GetTypeId(void)
+{
+	static TypeId tid = TypeId("ns3::Compressor<cx_double>")
+							.SetParent<Object>()
+							.AddConstructor<Compressor<cx_double>>()
+							.SetGroupName("CompressedSensing")
+							.AddAttribute("RanMatrix", "The underlying random matrix form to create the sensing matrix",
+										  TypeId::ATTR_SET,
+										  PointerValue(CreateObject<IdentRandomMatrix>()),
+										  MakePointerAccessor(&Compressor::SetRanMat),
+										  MakePointerChecker<RandomMatrix>())
+							.AddAttribute("TransMatrix", "The underlying matrix of a real transformation in which the solution is sparse",
+										  TypeId::ATTR_SET,
+										  PointerValue(),
+										  MakePointerAccessor(&Compressor::SetTransMat),
+										  MakePointerChecker<TransMatrix<cx_double>>());
+	return tid;
+}
+
+template <typename T>
+Compressor<T>::Compressor() : m_seed(1), m_m(0),
+							  m_n(0), m_vecLen(0),
+							  m_bufLenIn(0), m_bufLenOut(0)
+{
+}
+
+template <typename T>
+Compressor<T>::Compressor(uint32_t m, uint32_t n, uint32_t vecLen) : m_seed(1), m_m(m),
+																	 m_n(n), m_vecLen(vecLen),
+																	 m_bufLenIn(n * vecLen), m_bufLenOut(m * vecLen)
+{
+}
+
+template <typename T>
+void Compressor<T>::Setup(uint32_t seed, uint32_t m, uint32_t n, uint32_t vecLen, bool norm)
+{
+	NS_LOG_FUNCTION(this << seed << m << n << vecLen << norm);
+	m_seed = seed;
+	m_m = m;
+	m_n = n;
+	m_vecLen = vecLen;
+	m_bufLenIn = vecLen * n;
+	m_bufLenOut = vecLen * m;
+
+	if (m_transMat.isValid())
+		m_transMat->SetSize(n);
+
+	if (m_ranMat.isValid())
+		m_ranMat->SetSize(m, n, seed);
+
+	if (norm)
+		m_ranMat->NormalizeToM();
+}
+
+template <typename T>
+void Compressor<T>::Compress(const T *bufferIn, uint32_t bufLenIn, T *bufferOut, uint32_t bufLenOut)
+{
+	NS_LOG_FUNCTION(this << bufferIn << bufLenIn << bufferOut << bufLenOut);
+	NS_ASSERT_MSG(bufLenIn == m_bufLenIn, "Incorrect input buffer size");
+	NS_ASSERT_MSG(bufLenOut == m_bufLenOut, "Incorrect output buffer size");
+
+	klab::TSmartPointer<kl1p::TOperator<T>> op_ptr = m_ranMat * m_transMat;
+	const arma::Mat<T> x(const_cast<T *>(bufferIn), m_n, m_vecLen, false);
+	arma::Mat<T> y(bufferOut, m_m, m_vecLen, false);
+
+	for (uint32_t i = 0; i < m_vecLen; i++)
+	{
+		Col<T> yVec(m_m);
+		op_ptr->apply(x.col(i), yVec);
+		y.col(i) = yVec;
+	}
+}
+
+template <typename T>
+void Compressor<T>::SetRanMat(Ptr<RandomMatrix> ranMat_ptr)
+{
+	m_ranMat = ranMat_ptr->Clone();
+	
+	if (m_ranMat.isValid())
+		m_ranMat->SetSize(m_m, m_n, m_seed);
+}
+
+template <typename T>
+void Compressor<T>::SetTransMat(Ptr<TransMatrix<T>> transMat_ptr)
+{
+	m_transMat = transMat_ptr->Clone();
+
+	if (m_transMat.isValid())
+		m_transMat->SetSize(m_n);
+}
+
+template class Compressor<double>;
+template class Compressor<cx_double>;
---
+++ src/compressed-sensing/model/compressor.h
@@ -0,0 +1,99 @@
+/**
+* \file compressor.h
+*
+* \author Tobias Waurick
+* \date 11.07.17
+*
+*/
+
+#ifndef COMPRESSOR_H
+#define COMPRESSOR_H
+
+#include "ns3/core-module.h"
+#include <KL1pInclude.h>
+#include "node-data-buffer.h"
+#include "random-matrix.h"
+#include "transform-matrix.h"
+using namespace ns3;
+
+/**
+* \class Compressor	
+*
+* compresses measurement vectors X into a lower dimensional space Y
+*
+* \tparam T type of internal data (either double or arma::cx_double)
+*
+* \author Tobias Waurick
+* \date 11.07.17
+*/
+template <typename T>
+class Compressor : public Object
+{
+  public:
+	static TypeId GetTypeId(void);
+	Compressor();
+
+	/**
+	* \brief creates a compressor and sets the dimension for X and Y 
+	*
+	* \param n 		NOF original measurment vectors (X)
+	* \param m  	NOF  measurments vectors when compreesed
+	* \param vecLen	length of each measurement vector (e.g. 1 for temporal reconstruction, x for spatial reconstruction)
+	*
+	*/
+	Compressor(uint32_t m, uint32_t n, uint32_t vecLen);
+
+	/**
+	* \brief sets the dimension for X and Y 
+	*
+	* \param n 		NOF original measurment vectors (X)
+	* \param m  	NOF  measurments vectors when compreesed
+	* \param vecLen	length of each measurement vector (e.g. 1 for temporal reconstruction, x for spatial reconstruction)
+	* \param norm	normalize random matrix to 1/sqrt(m)?
+	*
+	*/
+	void Setup(uint32_t seed, uint32_t m, uint32_t n, uint32_t vecLen, bool norm = false);
+
+	/**
+	* \brief compresses data from input buffer and writes it to the output buffer
+	* If X is a matrix, column-wise order in the input buffer is assumed. In this case the output buffer is also written in acolumn-wise order
+	*
+	* \param bufferIn pointer to input buffer 
+	* \param bufLenIn length of input buffer (must be vecLen*n)
+	* \param bufferOut pointer to output buffer
+	* \param bufLenOut length of output buffer (must be vecLen*m)
+	*
+	*/
+	void Compress(const T *bufferIn, uint32_t bufLenIn, T *bufferOut, uint32_t bufLenOut);
+
+	/**
+	* \brief sets the internal random matrix object
+	*
+	* \param ranMat_ptr pointer to a RandomMatrix object
+	*
+	*/
+	void SetRanMat(Ptr<RandomMatrix> ranMat_ptr);
+
+	/**
+	* \brief sets the internal transformation object
+	*
+	* \param trans Mat_ptr pointer to a TransMatrix object
+	*
+	*/
+	void SetTransMat(Ptr<TransMatrix<T>> transMat_ptr);
+
+  private:
+
+	uint32_t
+		m_seed,										/**< seed to create random matrix from*/
+		m_m,										/**< length of Y*/
+		m_n,										/**< length of X*/
+		m_vecLen;									/**< size of one measurement vector*/
+	uint64_t m_bufLenIn,							/**< input buffer length */
+		m_bufLenOut;								/**< output buffer length */
+	bool m_normalize;								/**< normalize random matrix?*/
+	klab::TSmartPointer<RandomMatrix> m_ranMat;		/**< Random matrix form from which sensing matrix is constructed*/
+	klab::TSmartPointer<TransMatrix<T>> m_transMat; /**< Transformation matrix form from which sensing matrix is constructed*/
+};
+
+#endif //COMPRESSOR_H
