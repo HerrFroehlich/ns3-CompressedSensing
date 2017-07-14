0e4f654e8fbc7c8d1290bb22678d06c3af88fdcc
Tobias Waurick <s1550973@tu-dresden.de>
2017-07-14 18:29:44 +0200

    Added source app (doing temporal compression)

--- src/compressed-sensing/model/cs-src-app.cc
+++ src/compressed-sensing/model/cs-src-app.cc
@@ -26,10 +26,10 @@ CsSrcApp::GetTypeId(void)
 										  TimeValue(MilliSeconds(1)),
 										  MakeTimeAccessor(&CsSrcApp::m_interval),
 										  MakeTimeChecker())
-							.AddAttribute("PacketSize", "Maximum size of outbound packets' payload in bytes",
-										  UintegerValue(8),
-										  MakeUintegerAccessor(&CsSrcApp::m_packetSize),
-										  MakeUintegerChecker<uint32_t>(1))
+							// .AddAttribute("PacketSize", "Maximum size of outbound packets' payload in bytes",
+							// 			  UintegerValue(8),
+							// 			  MakeUintegerAccessor(&CsSrcApp::m_packetSize),
+							// 			  MakeUintegerChecker<uint32_t>(8))
 							.AddAttribute("m", "Length of original measurement vector",
 										  UintegerValue(128),
 										  MakeUintegerAccessor(&CsSrcApp::m_m),
@@ -42,8 +42,12 @@ CsSrcApp::GetTypeId(void)
 										  DoubleValue(1.0),
 										  MakeDoubleAccessor(&CsSrcApp::m_txProb),
 										  MakeDoubleChecker<double>(0.0, 1.0))
-							.AddAttribute("RanVar", "The random variable attached to create package data.",
-										  TypeId::ATTR_GET,
+							.AddAttribute("ComprTemp", "Temporal Compressor",
+										  PointerValue(CreateObject<CompressorTemp<double>>()),
+										  MakePointerAccessor(&CsSrcApp::m_compR),
+										  MakePointerChecker<CompressorTemp<double>>())
+							.AddAttribute("RanTx", "The random variable attached to determine when to send.",
+										  TypeId::ATTR_GET | TypeId::ATTR_CONSTRUCT,
 										  StringValue("ns3::UniformRandomVariable[Min=0.0|Max=1.0]"),
 										  MakePointerAccessor(&CsSrcApp::m_ranTx),
 										  MakePointerChecker<RandomVariableStream>())
@@ -53,25 +57,32 @@ CsSrcApp::GetTypeId(void)
 	return tid;
 }
 
-CsSrcApp::CsSrcApp() : m_nodeId(0), m_clusterId(0), m_seed(0),
-					   m_n(0), m_m(0), m_nDevices(0), m_nTxDevices(0),
-					   m_nMeas(0), m_nPackets(0), m_sent(0),
+CsSrcApp::CsSrcApp() : m_yR(0), m_nodeId(0), m_clusterId(0),
+					   m_nextSeq(0), m_seed(1),
+					   m_n(0), m_m(0),
+					   m_nDevices(0), m_nTxDevices(0),
+					   m_nMeas(0), // m_nPackets(0),
+					   m_sent(0),
 					   m_normalize(false),
 					   m_running(false),
 					   m_isSetup(false),
+					   //    m_txPackets(0),
 					   m_sendEvent(EventId())
 
 {
 	NS_LOG_FUNCTION(this);
 }
 
-CsSrcApp::CsSrcApp(uint32_t seed, uint32_t n, uint32_t m) : m_nodeId(0), m_clusterId(0), m_seed(seed),
-															m_n(n), m_m(m), m_nDevices(0), m_nTxDevices(0),
-															m_nMeas(0), m_nPackets(0), m_sent(0),
+CsSrcApp::CsSrcApp(uint32_t seed, uint32_t n, uint32_t m) : m_yR(m), m_nodeId(0), m_clusterId(0),
+															m_nextSeq(0), m_seed(seed),
+															m_n(n), m_m(m),
+															m_nDevices(0), m_nTxDevices(0),
+															m_nMeas(0), // m_nPackets(0),
+															m_sent(0),
 															m_normalize(false),
 															m_running(false),
 															m_isSetup(false),
-															m_yR(m),
+															// m_txPackets(0),
 															m_sendEvent(EventId())
 {
 	NS_LOG_FUNCTION(this << seed << n << m);
@@ -110,14 +121,10 @@ void CsSrcApp::Setup(Ptr<Node> node, CsHeader::T_IdField nodeId, CsHeader::T_IdF
 
 	//write to serial buffer
 	// m_fdata.Resize(flen);
-	m_fdata.MoveMem(buffer, flen);
+	m_fdata.MoveMem(buffer, bufSize);
 
 	// delete[] buffer;
 	// buffer = 0;
-
-	/*--------  calculating NOF packets needed  --------*/
-
-	m_nPackets = 1 + (bufSize - 1) / m_packetSize; // ceil
 
 	/*--------  get tx devices from node  --------*/
 	m_nDevices = node->GetNDevices();
@@ -131,13 +138,16 @@ void CsSrcApp::Setup(Ptr<Node> node, CsHeader::T_IdField nodeId, CsHeader::T_IdF
 		m_isTxDevice[i] = i;
 	}
 
+	//setup compressor
+	m_compR->Setup(m_seed, m_n, m_m, m_normalize);
+
 	m_isSetup = true;
 }
 
 void CsSrcApp::SetTempCompressor(Ptr<CompressorTemp<double>> comp)
 {
-
 	NS_LOG_FUNCTION(this << comp);
+
 	m_compR = comp;
 	m_compR->Setup(m_seed, m_n, m_m, m_normalize);
 }
@@ -145,6 +155,7 @@ void CsSrcApp::SetTempCompressor(Ptr<CompressorTemp<double>> comp)
 void CsSrcApp::SetTempCompressor(Ptr<CompressorTemp<double>> comp, uint32_t seed, uint32_t n, uint32_t m, bool norm)
 {
 	NS_LOG_FUNCTION(this << comp << seed << n << m << norm);
+
 	m_compR = comp;
 	m_seed = seed;
 	m_n = n;
@@ -157,32 +168,42 @@ void CsSrcApp::SetTempCompressor(Ptr<CompressorTemp<double>> comp, uint32_t seed
 void CsSrcApp::SetTempCompressDim(uint32_t n, uint32_t m)
 {
 	NS_LOG_FUNCTION(this << n << m);
+
 	m_n = n;
 	m_m = m;
 
-	m_yR.Resize(m);
+	// m_yR.Resize(m);
 	m_compR->Setup(m_seed, m_n, m_m, m_normalize);
 }
 
 void CsSrcApp::SetSeed(uint32_t seed, bool norm)
 {
 	NS_LOG_FUNCTION(this << seed << norm);
+
 	m_compR->SetSeed(seed, norm);
 }
 
 void CsSrcApp::SetTxProb(double p)
 {
 	NS_LOG_FUNCTION(this << p);
+
 	m_txProb = p;
 }
 
 void CsSrcApp::StartApplication()
 {
 	NS_LOG_FUNCTION(this);
+
 	NS_ASSERT_MSG(m_isSetup, "Run Setup first!");
 
+	/*--------  create all src packets --------*/
+	while (CompressNext())
+	{
+		CreateCsPackets();
+	}
+
 	m_running = true;
-	if (m_nPackets > 0)
+	if (HasPackets())
 	{
 		ScheduleTx(MilliSeconds(0.0));
 	}
@@ -190,57 +211,116 @@ void CsSrcApp::StartApplication()
 
 void CsSrcApp::StopApplication()
 {
+	NS_LOG_FUNCTION(this);
+
 	Simulator::Cancel(m_sendEvent);
+	m_running = false;
 }
 
-Ptr<Packet> CsSrcApp::CreateCsPackets()
-{
-	return Ptr<Packet>();
-}
-
-void CsSrcApp::CompressNext()
-{
-	uint32_t remain = m_fdata.GetRemaining();
-	double *yData = new double[m_m];
-
-	if (remain < m_n) // this actually shouldn't happen if nPackets is calculated right, but just to be sure
-	{
-		NS_LOG_WARN("Not enough samples left in file, sending zeros!");
-		std::fill(yData, yData + m_m, 0);
-	}
-	else
-	{
-		double xData[m_n];
-		m_fdata.ReadNext(xData, m_n);
-		m_compR->Compress(xData, m_n, yData, m_m);
-	}
-
-	m_yR.MoveMem(yData, m_m);
-}
-
-void CsSrcApp::WriteTxPacketList(std::vector<Ptr<Packet>> &pktList)
-{
-	m_txPackets.insert(m_txPackets.end(), pktList.begin(), pktList.end());
-}
-
-void CsSrcApp::SendPacket(Ptr<Packet> p)
+void CsSrcApp::SendToAll(Ptr<Packet> p)
 {
 	NS_LOG_FUNCTION(this << p);
+	NS_ASSERT(m_sendEvent.IsExpired());
+
+	Ptr<NetDevice> device;
 
 	//call trace source
 	NS_LOG_INFO(m_node->GetId() << " is about to send");
 	m_txTrace(p);
 
-	Ptr<NetDevice> device;
 	for (uint32_t i = 0; i < m_nTxDevices; i++)
 	{
 		device = m_node->GetDevice(m_isTxDevice[i]);
 		//send assuming MySimpleNetDevice thus invalid Address
 		device->Send(p, Address(), 0);
 	}
+}
+
+bool CsSrcApp::CompressNext()
+{
+	NS_LOG_FUNCTION(this);
+
+	/*--------  Compress next Y  --------*/
+
+	uint32_t remain = m_fdata.GetRemaining();
+
+	if (!m_compR)
+	{
+		NS_LOG_ERROR("Src Node" << int(m_nodeId) << " has no valid compressor attached!");
+		return false;
+	}
+	else if (remain < m_n)
+	{
+		// NS_LOG_WARN("Not enough samples left in file, sending zeros!");
+		// std::fill(yData, yData + m_m, 0);
+		NS_LOG_INFO("Src Node" << int(m_nodeId) << " has no more samples to compress!");
+		return false;
+	}
+
+	double xData[m_n];
+	double *yData = new double[m_m];
+	m_fdata.ReadNext(xData, m_n);
+	m_compR->Compress(xData, m_n, yData, m_m);
+
+	m_yR.MoveMem(yData, m_m);
+
+	return true;
+}
+
+void CsSrcApp::CreateCsPackets()
+{
+	/*--------  Create packets from that data  --------*/
+	// uint32_t nPacketsNow = 1;
+	std::vector<Ptr<Packet>> pktList;
+
+	uint32_t payloadSize = GetMaxPayloadSize();
+
+	CsHeader header;
+	header.SetClusterId(m_clusterId);
+	header.SetNodeId(m_nodeId);
+	header.SetSeq(m_nextSeq);
+	header.SetDataSize(payloadSize);
+	// double yData[m_m];
+	// m_yR.ReadNext(yData, m_m);
+
+	// Ptr<Packet> p = Create<Packet>(reinterpret_cast<uint8_t *>(yData), payloadSize);
+	Ptr<Packet> p = Create<Packet>(reinterpret_cast<const uint8_t *>(m_yR.GetMem()), payloadSize);
+
+	pktList.push_back(p);
+	WriteTxPacketList(pktList);
+	/*--------  Update members  --------*/
+	m_nextSeq++;
+}
+
+void CsSrcApp::WriteTxPacketList(const std::vector<Ptr<Packet>> &pktList)
+{
+	NS_LOG_FUNCTION(this);
+	if (HasPackets())
+		m_txPackets.insert(m_txPackets.begin() + pktList.size(), pktList.begin(), pktList.end());
+	else
+		m_txPackets = pktList;
+}
+
+uint32_t CsSrcApp::GetMaxPayloadSize()
+{
+	NS_LOG_FUNCTION(this);
+
+	return m_m * sizeof(double);
+}
+
+bool CsSrcApp::HasPackets()
+{
+	return !m_txPackets.empty();
+}
+
+void CsSrcApp::SendPacket(Ptr<Packet> p)
+{
+	NS_LOG_FUNCTION(this << p);
+
+	SendToAll(p);
 	// new tx?
 	m_sent++;
-	if (m_sent < m_nPackets)
+	if (HasPackets())
 	{
 		ScheduleTx(m_interval);
 	}
@@ -249,7 +329,12 @@ void CsSrcApp::SendPacket(Ptr<Packet> p)
 void CsSrcApp::ScheduleTx(Time dt)
 {
 	NS_LOG_FUNCTION(this << dt);
-	Ptr<Packet> pkt = CreateCsPacket();
+	NS_ASSERT_MSG(HasPackets(), "No packets to schedule!");
+
+	Ptr<Packet> pkt = m_txPackets.back();
+	m_txPackets.pop_back();
+
 	//schedule send
-	m_sendEvent = Simulator::Schedule(dt, &CsSrcApp::SendPacket, this, pkt);
+	if (m_ranTx->GetValue() < m_txProb)
+		m_sendEvent = Simulator::Schedule(dt, &CsSrcApp::SendPacket, this, pkt);
 }
---
+++ src/compressed-sensing/model/cs-src-app.h
@@ -0,0 +1,215 @@
+/**
+* \file cs-src-app.h
+*
+* \author Tobias Waurick
+* \date 12.07.17
+*
+*/
+#ifndef CS_SRCAPP_H
+#define CS_SRCAPP_H
+#include "ns3/core-module.h"
+#include "ns3/network-module.h"
+#include "ns3/applications-module.h"
+#include "cs-header.h"
+#include "compressor.h"
+#include "serial-buffer.h"
+
+using namespace ns3;
+
+/**
+* \ingroup compsens
+* \class CsSrcApp
+*
+* \brief A source app to compress data from a file temporally and transmitting it
+*
+* When running the setup this application reads data from a file specified by a file path and stores it locally.
+* Upon starting the application the n data samples are compressed to m with the help of the Compressor class and a given seed.
+* Then packets are formed containing the CsHeader and m samples as payload (so package loss won't corrupt a data vector).
+* Finally the application transmits them using no protocol at all (-> assuming use of classes from the simple-network module) 
+* with a certain time gap between each packet.
+*/
+class CsSrcApp : public Application
+{
+  public:
+	typedef double T_PktData; /**< package data type*/
+
+	static TypeId GetTypeId(void);
+
+	/**
+	* \brief create an CsSrcApp with default values
+	*/
+	CsSrcApp();
+
+	/**
+	* \brief create an CsSrcApp
+	*
+	* \param seed seed used for constructing the random matrix
+	* \param n length of original measurement vector
+	* \param m length of compressed vector
+	*/
+	CsSrcApp(uint32_t seed, uint32_t n, uint32_t m);
+
+	/**
+	* \brief setups the application to send packets with data from a file
+	* All aggregated devices to the node are considered Tx devices.  
+	* This function has to be called BEFORE starting the application. 
+	*
+	* \param node node to aggregate application to
+	* \param nodeId node id as defined in CsHeader
+	* \param clusterId cluster id as defined in CsHeader
+	* \param filename name of file to read from
+	*
+	*/
+	void Setup(Ptr<Node> node,
+			   CsHeader::T_IdField nodeId,
+			   CsHeader::T_IdField clusterId,
+			   std::string filename);
+
+	/**
+	* \brief sets the used temporal compressor
+	*  It is setuped with the previously defined/default seed and sizes 
+	* \param comp  pointer to compressor
+	*/
+	void SetTempCompressor(Ptr<CompressorTemp<double>> comp);
+
+	/**
+	* \brief sets the used temporal compressor
+	*  It is setuped via the given seed and sizes
+	*
+	* \param comp  pointer to compressor
+	* \param seed seed to use
+	* \param n length of original measurement vector
+	* \param m length of compressed vector
+	* \param norm normalize random matrix by 1/sqrt(m)?
+	*/
+	void SetTempCompressor(Ptr<CompressorTemp<double>> comp, uint32_t seed, uint32_t n, uint32_t m, bool norm = false);
+
+	/**
+	* \brief sets the compression given by n and m
+
+	* \param n length of original measurement vector
+	* \param m length of compressed vector
+	*/
+	void SetTempCompressDim(uint32_t n, uint32_t m);
+
+	/**
+	* \brief sets the seed used for generating the random sensing matrix
+	*
+	* \param seed seed to use
+	* \param norm normalize random matrix by 1/sqrt(m)?
+	*
+	*/
+	void SetSeed(uint32_t seed, bool norm = false);
+
+	/**
+	* \brief sets the transmission probability for sending
+	*
+	* \param p probability to send
+	*
+	*/
+	void SetTxProb(double p);
+
+	//inherited from Application
+	virtual void StartApplication();
+	virtual void StopApplication();
+
+  protected:
+	/**
+	* \brief send a packet via all tx devices
+	*
+	* \param p packet to send
+	*/
+	void SendToAll(Ptr<Packet> p);
+
+	/**
+	* \brief tries to compress the next Y and with that  to 
+	* The base class does temporal compression here. 
+	*
+	* \return true when a new Y could be compressed
+	*/
+	virtual bool CompressNext();
+
+	/**
+	* \brief create new packets with a CsHeader and payload
+	*/
+	virtual void CreateCsPackets();
+
+	/**
+	* \brief writes to the TX packet list 
+	* The last package in the vector will be send first
+	*
+	* \param pktList vector containing packets (Ptr<Packet>), which will be transmitted
+	*
+	*/
+	void WriteTxPacketList(const std::vector<Ptr<Packet>> &pktList);
+
+	/**
+	* \brief gets the maximum payload size
+	* CsSrcApp will packets will have a fixed sized of m*size(T_PktData), so that one compressed measurement fits in one packet.
+	* So this function provides the option for subclasses to calculate alternative sizes (e.g splitting Y over several packets).
+	* 
+	* \param pktSize maximum packet size
+	*
+	* \return calculated packetSize
+	*/
+	virtual uint32_t GetMaxPayloadSize();
+
+	/**
+	* \brief checks if has queued packets
+	*	
+	* \return true if there are packets queued
+	*/
+	bool HasPackets();
+
+	/**
+	* \brief schedules a transmit event for the simulator
+	*
+	* \param dt Time to next transmit event   
+	*
+	*/
+	void ScheduleTx(Time dt);
+
+	SerialDataBuffer<double> m_yR;		  /**< buffers for  compressed real meas. vector */
+	CsHeader::T_IdField m_nodeId, m_clusterId;
+	CsHeader::T_SeqField m_nextSeq; /**< next sequence!*/
+  private:
+	/**
+	* \brief sends a packet with compressed source data  via all devices in TX-device list
+	* if there are packets remaining in m_txPackets this will call another ScheduleTx
+	*
+	* \param p packet to send
+	*
+	*/
+	void SendPacket(Ptr<Packet> p);
+
+	Ptr<Node> m_node; /**< aggretated node*/
+
+	uint32_t
+		m_seed,		  /**< seed used for generating the random sensing matrix*/
+		m_n,		  /**< length of an original measurement vector*/
+		m_m,		  /**< length of compressed measurment vector*/
+		m_nDevices,   /**< number of net devices*/
+		m_nTxDevices, /**< number of net devices*/
+					  //	m_packetSize, /**< Packet size in byte*/
+		m_nMeas,	  /**< NOF measurements to send*/
+		//m_nPackets,   /**< NOF packets to send*/
+		m_sent; /**< NOF packets already sent*/
+
+	double m_txProb; /**< description*/
+
+	bool m_normalize, /**< normalize random matrix by 1/sqrt(m)?*/
+		m_running,
+		m_isSetup;
+
+	SerialDataBuffer<double> m_fdata; /**< data from file*/
+	Ptr<CompressorTemp<double>> m_compR;  /**< compressor for real*/
+	Ptr<RandomVariableStream> m_ranTx;	/**< random variable stream, to determine when to send*/
+	std::vector<uint32_t> m_isTxDevice;   /**< determine if device is used for sending */
+	std::vector<Ptr<Packet>> m_txPackets; /**< packets to send next, LIFO!*/
+
+	Time m_interval; /**< Packet inter-send time*/
+	EventId m_sendEvent;
+	TracedCallback<Ptr<const Packet>> m_txTrace; /**< callback to call when sending*/
+};
+
+#endif //CS_SRCAPP_H
