1d0aa0a214d8cd9c4c4ccee79aad35366d5ef1d4
Tobias Waurick <s1550973@tu-dresden.de>
2017-07-15 16:49:28 +0200

    Fixes & Extensions

--- src/compressed-sensing/model/serial-buffer.h
+++ src/compressed-sensing/model/serial-buffer.h
@@ -32,7 +32,7 @@
 * \date 30.05.17
 */
 template <typename T>
-class SerialDataBuffer<T> : public Object
+class SerialDataBuffer : public Object
 {
   public:
 	/**
@@ -55,7 +55,16 @@ class SerialDataBuffer<T> : public Object
 	* \param buf buffer to copy
 	*
 	*/
-	SerialDataBuffer(const SerialDataBuffer& buf);
+	SerialDataBuffer(const SerialDataBuffer &buf);
+
+	/**
+	* \brief reates a SerialDataBuffer and moves the memory from the buffer
+	* The buffer pointer will be set to zero after memory has been moved, to avoid delete conflicts.
+	*
+	* \param buf buffer to copy
+	*
+	*/
+	SerialDataBuffer(T *&buffer, uint32_t bufSize);
 
 	/**
 	* \brief destroy the Buffer
@@ -70,7 +79,7 @@ class SerialDataBuffer<T> : public Object
 	*
 	* \return indexed value in buffer
 	*/
-	T Read(uint32_t index);
+	T Read(uint32_t index) const;
 
 	/**
 	* \brief reads multiple values starting at at an index to a buffer 
@@ -79,14 +88,14 @@ class SerialDataBuffer<T> : public Object
 	* \param buffer	a pointer to a  buffer  of the stored type
 	* \param bufSize	size of that buffer
 	*/
-	void Read(uint32_t index, *buffer, uint32_t bufSize);
+	void Read(uint32_t index, T *buffer, uint32_t bufSize) const;
 
 	/**
 	* \brief reads next value this SerialDataBuffer
 	*
 	* \return next value in SerialDataBuffer
 	*/
-	T ReadNext();
+	T ReadNext() const;
 
 	/**
 	* \brief reads multiple next values in a buffer given by a pointer
@@ -94,7 +103,26 @@ class SerialDataBuffer<T> : public Object
 	* \param buffer	a pointer to a  buffer  of the stored type
 	* \param bufSize	size of that buffer
 	*/
-	void ReadNext(T *SerialDataBuffer, uint32_t bufSize);
+	void ReadNext(T *buffer, uint32_t bufSize) const;
+
+	/**
+	* \brief Moves the memory from a buffer to this serial buffer
+	* BE SURE TO ALLOCATE THE BUFFER ON THE HEAP!
+	* The buffer pointer will be set to zero after memory has been moved, to avoid delete conflicts.
+	*
+	* \param buffer 	a reference to a pointer to a  buffer  of the stored type
+	* \param bufSize	size of that buffer
+	*
+	* \return returnDesc
+	*/
+	void MoveMem(T *&buffer, uint32_t bufSize);
+
+	/**
+	* \brief gets a constant pointer to underlying memory
+	*
+	* \return  constant pointer to the memory
+	*/
+	const T *GetMem() const;
 
 	/**
 	* \brief writes a single value into this SerialDataBuffer
@@ -109,14 +137,14 @@ class SerialDataBuffer<T> : public Object
 	* \param SerialDataBuffer	a SerialDataBuffer pointer of the stored type
 	* \param bufSize	size of that SerialDataBuffer
 	*/
-	void WriteNext(T *SerialDataBuffer, uint32_t bufSize);
+	void WriteNext(T *buffer, uint32_t bufSize);
 
 	/**
 	* \brief determine if the buffer is full
 	*
 	* \return true if the buffer is full
 	*/
-	bool IsFull();
+	bool IsFull() const;
 
 	/**
 	* \brief clears the buffer by setting the write index to zero
@@ -130,14 +158,33 @@ class SerialDataBuffer<T> : public Object
 	* \param size size of new buffer
 	*
 	*/
-	void
-	Resize(uint32_t size);
+	void Resize(uint32_t size);
+
+	/**
+	* \brief gets the space remaining in the buffer for writting
+	* \return space
+	*/
+	uint32_t GetSpace() const;
+
+	/**
+	* \brief gets the remaining NOF elements for reading
+	*
+	* \return NOF remaining elements
+	*/
+	uint32_t GetRemaining() const;
+
+	/**
+	* \brief gets the total size of the buffer
+	*
+	* \return total size
+	*/
+	uint32_t GetSize() const;
 
   private:
-	T *m_data_ptr;		 /**< stored data*/
-	uint32_t m_dataSize, /**< size of stored data*/
-		m_wrIdx,		 /**< current write index*/
-		m_rdIdx;		 /**< current read index*/
+	T *m_data_ptr;			  /**< stored data*/
+	uint32_t m_dataSize,	  /**< size of stored data*/
+		m_wrIdx;			  /**< current write index*/
+	mutable uint32_t m_rdIdx; /**< current read index*/
 };
 
 template <typename T>
@@ -157,11 +204,17 @@ SerialDataBuffer<T>::SerialDataBuffer(uint32_t dataSize) : m_dataSize(dataSize),
 }
 
 template <typename T>
-SerialDataBuffer<T>::SerialDataBuffer(const SerialDataBuffer& buf)
+SerialDataBuffer<T>::SerialDataBuffer(const SerialDataBuffer &buf)
 {
 	m_dataSize = buf.m_dataSize;
 	m_data_ptr = new T[m_dataSize];
 	std::copy(std::begin(buf.m_data_ptr), std::end(buf.m_data_ptr), std::begin(m_data_ptr));
+}
+
+template <typename T>
+SerialDataBuffer<T>::SerialDataBuffer(T *&buffer, uint32_t bufSize)
+{
+	MoveMem(buffer, bufSize);
 }
 
 template <typename T>
@@ -173,7 +226,7 @@ SerialDataBuffer<T>::~SerialDataBuffer()
 }
 
 template <typename T>
-T SerialDataBuffer<T>::Read(uint32_t index)
+T SerialDataBuffer<T>::Read(uint32_t index) const
 {
 	NS_ASSERT_MSG(index <= m_wrIdx, "Read index > write index : Trying to read outside of written area!");
 
@@ -181,25 +234,43 @@ T SerialDataBuffer<T>::Read(uint32_t index)
 }
 
 template <typename T>
-void SerialDataBuffer<T>::Read(uint32_t index, T *buffer, uint32_t bufSize)
+void SerialDataBuffer<T>::Read(uint32_t index, T *buffer, uint32_t bufSize) const
 {
 	NS_ASSERT_MSG((index + bufSize) <= m_wrIdx,
 				  "Read index > write index : Trying to read outside of written area!");
 
-	memcpy(SerialDataBuffer, m_data_ptr + index, bufSize);
+	memcpy(buffer, m_data_ptr + index, bufSize);
 }
 
 template <typename T>
-T SerialDataBuffer<T>::ReadNext()
+T SerialDataBuffer<T>::ReadNext() const
 {
 	return Read(m_rdIdx++);
 }
 
 template <typename T>
-void SerialDataBuffer<T>::ReadNext(T *buffer, uint32_t bufSize)
+void SerialDataBuffer<T>::ReadNext(T *buffer, uint32_t bufSize) const
 {
-	Read(m_rdIdx, buffer, bufSize)
-		m_rdIdx += bufSize;
+	Read(m_rdIdx, buffer, bufSize);
+	m_rdIdx += bufSize;
+}
+
+template <typename T>
+void SerialDataBuffer<T>::MoveMem(T *&buffer, uint32_t bufSize)
+{
+	delete[] m_data_ptr;
+	m_dataSize = bufSize;
+	m_wrIdx = bufSize;
+	m_rdIdx = 0;
+
+	m_data_ptr = buffer;
+	buffer = 0;
+}
+
+template <typename T>
+const T *SerialDataBuffer<T>::GetMem() const
+{
+	return const_cast<const T *> (m_data_ptr);
 }
 
 template <typename T>
@@ -219,9 +290,9 @@ void SerialDataBuffer<T>::WriteNext(T *buffer, uint32_t bufSize)
 }
 
 template <typename T>
-bool SerialDataBuffer<T>::IsFull()
+bool SerialDataBuffer<T>::IsFull() const
 {
-	return (m_wrIdx + 1 == m_dataSize);
+	return (m_wrIdx == m_dataSize);
 }
 
 template <typename T>
@@ -239,6 +310,23 @@ void SerialDataBuffer<T>::Resize(uint32_t size)
 	m_wrIdx = 0;
 	m_rdIdx = 0;
 	m_data_ptr = new T[m_dataSize];
+}
+
+template <typename T>
+uint32_t SerialDataBuffer<T>::GetSpace() const
+{
+	return m_dataSize - m_wrIdx;
+}
+
+template <typename T>
+uint32_t SerialDataBuffer<T>::GetRemaining() const
+{
+	return m_wrIdx - m_rdIdx;
+}
+template <typename T>
+uint32_t SerialDataBuffer<T>::GetSize() const
+{
+	return m_dataSize;
 }
 
 #endif //SERIALBUFFER_H
