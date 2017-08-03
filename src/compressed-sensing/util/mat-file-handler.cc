d47d89f4b6acf2911ff348e63b4c003e9759ae8f
Tobias Waurick <s1550973@tu-dresden.de>
2017-08-03 14:27:27 +0200

    fixes, new functions

--- src/compressed-sensing/util/mat-file-handler.cc
+++ src/compressed-sensing/util/mat-file-handler.cc
@@ -51,13 +51,23 @@ bool MatFileHandler::Open(std::string path)
 	while ((matvar = Mat_VarReadNextInfo(m_matFile)) != NULL)
 	{
 		std::string name = matvar->name;
-		S_VAR_INFO info = CreateInfo(matvar);
-		m_varInfoMap[name] = info;
+		CreateInfo(name, matvar);
 		Mat_VarFree(matvar);
 		matvar = NULL;
 	}
 
 	return true;
+}
+
+void MatFileHandler::OpenNew(std::string path)
+{
+	//check if an other file was opened before
+	if (m_isOpen)
+		Mat_Close(m_matFile);
+
+	m_matFile = Mat_CreateVer(path.c_str(), NULL, MAT_VERSION);
+	NS_ASSERT_MSG(m_matFile != NULL, "Could not write file!");
+	m_isOpen = true;
 }
 
 void MatFileHandler::Close()
@@ -82,8 +92,7 @@ double MatFileHandler::ReadValue(std::string name) const
 
 	return *((double *)matvar->data);
 }
-
-template double MatFileHandler::ReadValue<double>(std::string name) const;
+template double MatFileHandler::ReadValue<double>(std::string) const;
 
 template <>
 DataStream<double> MatFileHandler::ReadMat(std::string name) const
@@ -92,7 +101,7 @@ DataStream<double> MatFileHandler::ReadMat(std::string name) const
 	matvar_t *matvar;
 
 	matvar = Mat_VarRead(m_matFile, name.c_str());
-	NS_ASSERT_MSG(matvar, "Variable does not exist!");
+	NS_ASSERT_MSG(matvar, "Variable " + name + " does not exist!");
 	NS_ASSERT_MSG(matvar->class_type == matio_classes::MAT_C_DOUBLE && !matvar->isComplex, "Variable is not a double!");
 	NS_ASSERT_MSG(matvar->rank == 2 && (matvar->dims[0] >= 1 && matvar->dims[1] >= 1),
 				  "Variable is not a vector/ matrix!");
@@ -114,34 +123,271 @@ DataStream<double> MatFileHandler::ReadMat(std::string name) const
 	Mat_VarFree(matvar);
 	return stream;
 }
-
-template DataStream<double> MatFileHandler::ReadMat<double>(std::string name) const;
+template DataStream<double> MatFileHandler::ReadMat<double>(std::string) const;
 
 template <>
 void MatFileHandler::WriteValue(std::string name, double value)
 {
 	NS_ASSERT_MSG(m_isOpen, "Open file first!");
-	NS_ASSERT_MSG(!m_varInfoMap.count(name), "Variable with this name already exists!");
+	//NS_ASSERT_MSG(!m_varInfoMap.count(name), "Variable with this name already exists!");
+	if (m_varInfoMap.count(name)) // overwrite existing
+	{
+		NS_LOG_WARN("Overwriting variable " + name);
+		Mat_VarDelete(m_matFile, name.c_str());
+	}
 
 	size_t dims[2] = {1, 1};
-	matvar_t *matvar = Mat_VarCreate(name.c_str(), MAT_C_DOUBLE, MAT_T_DOUBLE, 2, dims, &value,0);
+	matvar_t *matvar = Mat_VarCreate(name.c_str(), MAT_C_DOUBLE, MAT_T_DOUBLE, 2, dims, &value, 0);
 
 	NS_ASSERT_MSG(matvar, "Could not create variable!");
+	CreateInfo(name, matvar);
 
-	Mat_VarWrite(m_matFile, matvar, COMPRESSION_NONE);
+	Mat_VarWrite(m_matFile, matvar, MAT_COMPRESSION);
 	Mat_VarFree(matvar);
 }
-
 template void MatFileHandler::WriteValue<double>(std::string name, double);
 
 template <>
-void MatFileHandler::WriteMat(std::string name, const DataStream<double> &stream)
+void MatFileHandler::WriteMat(std::string name, const arma::Mat<double> &mat)
 {
+	NS_ASSERT_MSG(m_isOpen, "Open file first!");
+
+	//NS_ASSERT_MSG(!m_varInfoMap.count(name), "Variable with this name already exists!");
+	if (m_varInfoMap.count(name)) // overwrite existing
+	{
+		NS_LOG_WARN("Overwriting variable " + name);
+		Mat_VarDelete(m_matFile, name.c_str());
+	}
+
+	size_t dims[2] = {mat.n_rows, mat.n_cols};
+	void *data = (void *)const_cast<double *>(mat.memptr());
+	matvar_t *matvar = Mat_VarCreate(name.c_str(), MAT_C_DOUBLE, MAT_T_DOUBLE, 2, dims, data, MAT_F_DONT_COPY_DATA);
+	NS_ASSERT_MSG(matvar, "Could not create variable!");
+
+	Mat_VarWrite(m_matFile, matvar, MAT_COMPRESSION);
+	CreateInfo(name, matvar);
+	Mat_VarFree(matvar);
+}
+template void MatFileHandler::WriteMat<double>(std::string, const arma::Mat<double> &);
+
+template <>
+void MatFileHandler::WriteMat(const DataStream<double> &stream)
+{
+	NS_ASSERT_MSG(m_isOpen, "Open file first!");
+
+	std::string name = stream.GetName();
+	//NS_ASSERT_MSG(!m_varInfoMap.count(name), "Variable with this name already exists!");
+	if (m_varInfoMap.count(name)) // overwrite existing
+	{
+		NS_LOG_WARN("Overwriting variable " + name);
+		Mat_VarDelete(m_matFile, name.c_str());
+	}
+
+	uint32_t maxSize = stream.GetMaxSize();
+	size_t dims[2] = {maxSize, stream.GetN()};
+
+	double data[maxSize * stream.GetN()];
+	uint32_t writeIdx = 0;
+	for (auto it = stream.Begin(); it != stream.End(); it++)
+	{
+		double dataCol[maxSize] = {0.0};
+		uint32_t bufSize = (*it)->GetSize();
+		(*it)->Read(0, dataCol, bufSize);
+		std::copy(dataCol, dataCol + bufSize, data + writeIdx);
+		writeIdx += maxSize;
+	}
+
+	matvar_t *matvar = Mat_VarCreate(name.c_str(), MAT_C_DOUBLE, MAT_T_DOUBLE, 2, dims, &data, MAT_F_DONT_COPY_DATA);
+	NS_ASSERT_MSG(matvar, "Could not create variable!");
+
+	Mat_VarWrite(m_matFile, matvar, MAT_COMPRESSION);
+	CreateInfo(name, matvar);
+	Mat_VarFree(matvar);
+}
+template void MatFileHandler::WriteMat<double>(const DataStream<double> &);
+
+template <>
+matvar_t *MatFileHandler::CreateStructMatField(Ptr<DataStream<double>> stream)
+{
+	NS_ASSERT(stream); //null pointer check
+
+	// create field variable
+	matvar_t *matvar;
+	uint32_t maxSize = stream->GetMaxSize(),
+			 nBuf = stream->GetN();
+	size_t dims[2] = {maxSize, nBuf};
+
+	double data[maxSize * nBuf];
+	uint32_t writeIdx = 0;
+	for (auto it = stream->Begin(); it != stream->End(); it++)
+	{
+		double dataCol[maxSize] = {0.0};
+		uint32_t bufSize = (*it)->GetSize();
+		(*it)->Read(0, dataCol, bufSize);
+		std::copy(dataCol, dataCol + bufSize, data + writeIdx);
+		writeIdx += maxSize;
+	}
+	std::string name = stream->GetName();
+	matvar = Mat_VarCreate(name.c_str(), MAT_C_DOUBLE, MAT_T_DOUBLE, 2, dims, &data, 0);
+	NS_ASSERT_MSG(matvar, "Could not create variable " + name + "!");
+
+	return matvar;
+}
+template matvar_t *MatFileHandler::CreateStructMatField(Ptr<DataStream<double>>);
+
+template <>
+void MatFileHandler::WriteStruct(const DataStreamContainer<double> &container)
+{
+	NS_ASSERT_MSG(m_isOpen, "Open file first!");
+
+	std::string groupName = container.GetGroupName();
+
+	if (m_varInfoMap.count(groupName)) // overwrite existing
+	{
+		NS_LOG_WARN("Overwriting variable " + groupName);
+		Mat_VarDelete(m_matFile, groupName.c_str());
+	}
+
+	size_t nStreams = container.GetNStreams();
+	matvar_t *matvar, *field;
+	size_t structDims[2] = {1, 1};
+	std::vector<std::string> fieldnames_s(nStreams);
+	const char *fieldnames_c[nStreams];
+
+	//get field names
+	for (uint32_t i = 0; i < nStreams; i++)
+	{
+		// get name
+		Ptr<DataStream<double>> stream = container.GetStream(i);
+		fieldnames_s.at(i) = stream->GetName();
+		fieldnames_c[i] = fieldnames_s.at(i).c_str();
+	}
+
+	matvar = Mat_VarCreateStruct(groupName.c_str(), 2, structDims, fieldnames_c, nStreams);
+	NS_ASSERT_MSG(matvar, "Could not create structure!");
+	//add fields to structure
+	for (uint32_t i = 0; i < nStreams; i++)
+	{
+		Ptr<DataStream<double>> stream = container.GetStream(i);
+		// create field variable
+		field = CreateStructMatField(stream);
+
+		Mat_VarSetStructFieldByIndex(matvar, i, 0, field);
+	}
+
+	CreateInfo(groupName, matvar);
+	Mat_VarWrite(m_matFile, matvar, MAT_COMPRESSION);
+	Mat_VarFree(matvar);
+}
+template void MatFileHandler::WriteStruct<double>(const DataStreamContainer<double> &);
+
+void MatFileHandler::WriteCluster(const CsCluster &cluster)
+{
+	NS_ASSERT_MSG(m_isOpen, "Open file first!");
+
+	std::string groupName = cluster.GetGroupName();
+
+	if (m_varInfoMap.count(groupName)) // overwrite existing
+	{
+		NS_LOG_WARN("Overwriting variable " + groupName);
+		Mat_VarDelete(m_matFile, groupName.c_str());
+	}
+
+	uint32_t nStreamsCluster = cluster.GetNStreams(),
+			 nNodes = cluster.GetN(),
+			 nFields = nStreamsCluster + nNodes;
+
+	matvar_t *matvar, *field;
+	size_t structDims[2] = {1, 1};
+	std::vector<std::string> fieldnames_s(nFields);
+	const char *fieldnames_c[nFields];
+
+	//get field names for cluster data
+	for (uint32_t i = 0; i < nStreamsCluster; i++)
+	{
+		// get name
+		Ptr<DataStream<double>> stream = cluster.GetStream(i);
+		fieldnames_s.at(i) = stream->GetName();
+		fieldnames_c[i] = fieldnames_s.at(i).c_str();
+	}
+
+	//get field names for node data
+	uint32_t j = nStreamsCluster;
+	for (auto it = cluster.Begin(); it != cluster.End(); it++)
+	{
+		fieldnames_s.at(j) = (*it)->GetGroupName();
+		fieldnames_c[j] = fieldnames_s.at(j).c_str();
+		j++;
+	}
+
+	//create structure variable
+	matvar = Mat_VarCreateStruct(groupName.c_str(), 2, structDims, fieldnames_c, nFields);
+	NS_ASSERT_MSG(matvar, "Could not create structure!");
+
+	//add matrix fields to structure
+	for (uint32_t i = 0; i < nStreamsCluster; i++)
+	{
+		Ptr<DataStream<double>> stream = cluster.GetStream(i);
+		// create field variable
+		field = CreateStructMatField(stream);
+
+		Mat_VarSetStructFieldByIndex(matvar, i, 0, field);
+	}
+
+	//for each node add a new structure field
+	j = nStreamsCluster;
+	for (auto it = cluster.Begin(); it != cluster.End(); it++)
+	{
+		matvar_t *fieldNode;
+		uint32_t nStreamsNode = (*it)->GetNStreams();
+		std::vector<std::string> fieldnamesNode_s(nStreamsNode);
+		const char *fieldnamesNode_c[nStreamsNode];
+
+		//get field names
+		for (uint32_t i = 0; i < nStreamsNode; i++)
+		{
+			// get name
+			Ptr<DataStream<double>> stream = (*it)->GetStream(i);
+			fieldnamesNode_s.at(i) = stream->GetName();
+			fieldnamesNode_c[i] = fieldnamesNode_s.at(i).c_str();
+		}
+
+		field = Mat_VarCreateStruct(fieldnames_c[j], 2, structDims, fieldnamesNode_c, nStreamsNode);
+		NS_ASSERT_MSG(field, "Could not create structure!");
+		//add fields to structure
+		for (uint32_t i = 0; i < nStreamsNode; i++)
+		{
+			Ptr<DataStream<double>> stream = (*it)->GetStream(i);
+			// create field variable
+			fieldNode = CreateStructMatField(stream);
+
+			Mat_VarSetStructFieldByIndex(field, i, 0, fieldNode);
+		}
+		Mat_VarSetStructFieldByIndex(matvar, j++, 0, field);
+	}
+
+	CreateInfo(groupName, matvar);
+	Mat_VarWrite(m_matFile, matvar, MAT_COMPRESSION);
+	Mat_VarFree(matvar);
 }
 
-template void MatFileHandler::WriteMat<double>(std::string name, const DataStream<double> &stream);
+std::vector<std::string> MatFileHandler::GetVarNames()
+{
+	std::vector<std::string> retval;
+	for (auto const &element : m_varInfoMap)
+	{
+		retval.push_back(element.first);
+	}
+	return retval;
+}
 
-MatFileHandler::S_VAR_INFO MatFileHandler::CreateInfo(const matvar_t *matvar) const
+MatFileHandler::S_VAR_INFO MatFileHandler::GetVarInfo(std::string name)
+{
+	NS_ASSERT_MSG(m_varInfoMap.count(name), "Variable with this name doesn't exists!");
+	return m_varInfoMap.at(name);
+}
+
+void MatFileHandler::CreateInfo(std::string name, const matvar_t *matvar)
 {
 	NS_ASSERT(matvar); //null pointer check
 
@@ -164,5 +410,5 @@ MatFileHandler::S_VAR_INFO MatFileHandler::CreateInfo(const matvar_t *matvar) co
 		info.nElem = info.dim[0] * info.dim[1] * info.dim[2];
 	}
 
-	return info;
+	m_varInfoMap[name] = info;
 }
--- src/compressed-sensing/util/mat-file-handler.h
+++ src/compressed-sensing/util/mat-file-handler.h
@@ -10,9 +10,11 @@
 
 #include <string>
 #include <map>
+#include <armadillo>
 #include <matio.h>
-#include "matio.h"
 #include "ns3/object.h"
+#include "ns3/ptr.h"
+#include "ns3/cs-cluster.h"
 #include "data-stream.h"
 #include "template-registration.h"
 using namespace ns3;
@@ -26,8 +28,10 @@ using namespace ns3;
 * For later data type extensions the various read and write operations are templated.
 * So far only  uncompressed version 5 mat files are supported by this class. 
 * Therefore when saving from MATLAB assure to call save with the '-v6' flag.
-* If for a future version version 7.3 files should be supported change the typedef
+* If for a future version version 7.3 files should be supported change the constant
 * MAT_VERSION to MAT_FT_MAT73 and build the MATIO library together with the HDF5 library.
+* Furthermore when variables should be written compressed, it is necessary to build with the zlib library
+* and set the constant MAT_COMPRESSION to MAT_COMPRESSION_ZLIB.
 *
 */
 class MatFileHandler : public Object
@@ -71,6 +75,15 @@ class MatFileHandler : public Object
 	bool Open(std::string path);
 
 	/**
+	* \brief creates a new mat file at the given path
+	*
+	* \param dir path to mat file
+	*
+	* \return true if mat file existed beforehand
+	*/
+	void OpenNew(std::string path);
+
+	/**
 	* \brief closes the mat file if open
 	*/
 	void Close();
@@ -106,7 +119,7 @@ class MatFileHandler : public Object
 	/**
 	* \brief writes a value to the mat file
 	*
-	* Asserts that a file has been opened yet, that the variable was created succesfully and the name is unique
+	* Asserts that a file has been opened yet, that the variable was created succesfully.
 	*
 	* \param name name of new variable
 	* \param value value to write
@@ -117,31 +130,115 @@ class MatFileHandler : public Object
 	void WriteValue(std::string name, T value);
 
 	/**
-	* \brief writes a MxN matrix to the mat file
+	* \brief writes a MxN matrix with the given name from a arma::Mat<T> to the mat file 
 	*
-	* Asserts that a file has been opened yet, that the variable was created succesfully and the name is unique
-	* Each SerialDataBuffer represents a column of the matrix. Thus the dimension of the matrix are determined
-	* by the maximum length of the SerialDataBuffer instances(M) and the amount of SerialDataBuffer instances(N).
-	*
-	* \param name name of new variable
-	* \param stream DataStream object containing the values to write
+	* \param name name of the variable to create
+	* \param mat arma::Mat<T> to write data from
 	* \tparam type of data (so far only double)
 	*
 	*/
 	template <typename T>
-	void WriteMat(std::string name, const DataStream<T> &stream);
-
-  private:
-	const static mat_ft MAT_VERSION = MAT_FT_MAT5; /**< version of the mat file */
+	void WriteMat(std::string name, const arma::Mat<T> &mat); 
 
 	/**
-	* \brief creates a S_VAR_INFO struct out of a matvar_t struct
+	* \brief interpretates the DataStream as a MxN matrix and writes to the mat file
 	*
+	* Asserts that a file has been opened yet, that the variable was created succesfully.
+	* Each SerialDataBuffer represents a column of the matrix. Thus the dimension of the matrix are determined
+	* by the maximum length of the SerialDataBuffer instances(M) and the amount of SerialDataBuffer instances(N).
+	* Missing values are patted with zeros.
+	*
+	* \param stream DataStream object containing the values to write and the name of the variable
+	* \tparam type of data (so far only double)
+	*
+	*/
+	template <typename T>
+	void WriteMat(const DataStream<T> &stream);
+
+	/**
+	* \brief writes mulitple MxN matrix to the mat file
+	*
+	* Calls WriteMat for each DataStream instance in the container
+	*
+	* \param container DataStreamContainer with multiple DataStream objects.
+	* \tparam type of data (so far only double)
+	*
+	*/
+	template <typename T>
+	void WriteMat(const DataStreamContainer<T> &container)
+	{
+		for (auto it = container.Begin(); it != container.End(); it++)
+		{
+			WriteMat<T>(*(*it));
+		}
+	};
+
+	/**
+	* \brief writes a MATLAB structure, which fields contain  MxN matrices
+	*
+	* The name of the variable is set from the group name of the DataStreamContainer.
+	* Each DataStream of the DataStreamContainer is seen as fields of the structure storing a MxN matrix.
+	* Each SerialDataBuffer represents a column of the matrix. Thus the dimension of the matrix are determined
+	* by the maximum length of the SerialDataBuffer instances(M) and the amount of SerialDataBuffer instances(N).
+	* Missing values are patted with zeros.
+	*
+	* \param streams DataStreamContainer from which structure is created
+	* \tparam type of data (so far only double)
+	*
+	*/
+	template <typename T>
+ 	void WriteStruct(const DataStreamContainer<T> &container);
+
+	/**
+	* \brief writes the data from a CsCluster and all its attached nodes
+	*
+	* All data is grouped in a single structure named after the group name of the CsCluster.
+	* Each field of the CsCluster and each CsNode is created in the same way as for WriteStruct(const DataStreamContainer<double> &container):
+	*
+	* \param cluster cluster for which data should be written	
+	*
+	*/
+	void WriteCluster(const CsCluster &cluster);
+
+	/**
+	* \brief gets all variable names stored in mat file
+	*
+	* \return vector with variable names
+	*/
+	std::vector<std::string> GetVarNames();
+
+	/**
+	* \brief gets the info for a specific variable name
+	*
+	* \return S_VAR_INFO struct with the variable's meta information
+	*/
+	S_VAR_INFO GetVarInfo(std::string name);
+
+  private:
+
+	/**
+	* \brief create a matrix field for a structure
+	*
+	*
+	* \param stream pointer to DataStream containing the data to write
+	* \return  pointer to matvar_t which should be written to
+	* \tparam type of data (so far only double)
+	*
+	*/
+	template <typename T>
+	matvar_t* CreateStructMatField(Ptr<DataStream<T>> stream);
+
+	const static mat_ft MAT_VERSION = MAT_FT_MAT5;											  /**< version of the mat file */
+	const static matio_compression MAT_COMPRESSION = matio_compression::MAT_COMPRESSION_NONE; /**< compression when writting variables*/
+
+	/**
+	* \brief creates a S_VAR_INFO struct out of a matvar_t struct and appends it to m_varInfoMap;
+	*
+	* \param unique name of variable
 	* \param matvar pointer to matvar_t struct
 	*
-	* \return filled S_VAR_INFO
 	*/
-	S_VAR_INFO CreateInfo(const matvar_t *matvar) const;
+	void CreateInfo(std::string name, const matvar_t *matvar);
 
 	mat_t *m_matFile; /**< info structure about opened file*/
 	bool m_isOpen;	/**< is a file stile open?*/
