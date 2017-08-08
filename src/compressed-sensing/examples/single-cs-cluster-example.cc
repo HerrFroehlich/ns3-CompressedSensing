b5b1ee533cbf545decc5a55692eab8cdea278c9b
Tobias Waurick <s1550973@tu-dresden.de>
2017-08-08 17:19:41 +0200

    Added more functionality

--- src/compressed-sensing/examples/single-cs-cluster-example.cc
+++ src/compressed-sensing/examples/single-cs-cluster-example.cc
@@ -26,13 +26,16 @@ using namespace std;
 NS_LOG_COMPONENT_DEFINE("SingleCsCluster");
 
 MatFileHandler matHandler_glob;
+bool verbose = false,
+	 info = false;
 
 static void
 compressCb(arma::Mat<double> matIn, arma::Mat<double> matOut)
 {
-
-	NS_LOG_INFO("\n"
-				<< Simulator::Now() << " Node " << Simulator::GetContext() << " compressed.");
+	NS_LOG_FUNCTION(&matIn << &matOut);
+	if (info || verbose)
+		cout << "\n"
+			 << Simulator::Now() << " Node " << Simulator::GetContext() << " compressed.";
 	static uint32_t count = 0;
 	matHandler_glob.WriteMat("notCompressSpat" + std::to_string(count), matIn);
 	matHandler_glob.WriteMat("CompressSpat" + std::to_string(count++), matOut);
@@ -42,8 +45,10 @@ static void
 receiveCb(Ptr<const Packet> p)
 {
 	NS_LOG_FUNCTION(p);
-	NS_LOG_INFO("\n"
-				<< Simulator::Now() << " Node " << Simulator::GetContext() << " Received:");
+
+	if (info || verbose)
+		cout << "\n"
+			 << Simulator::Now() << " Node " << Simulator::GetContext() << " Received:";
 	p->Print(cout);
 	cout << endl;
 }
@@ -52,17 +57,34 @@ static void
 transmittingCb(Ptr<const Packet> p)
 {
 	NS_LOG_FUNCTION(p);
-	NS_LOG_INFO("\n"
-				<< Simulator::Now() << " Node " << Simulator::GetContext() << " Sends:");
+
+	if (info || verbose)
+		cout << "\n"
+			 << Simulator::Now() << " Node " << Simulator::GetContext() << " Sends:";
 	p->Print(cout);
 	cout << endl;
 }
 
-// static void
-// recErrorCb(const klab::KException &e)
-// {
-// 	NS_LOG_ERROR("Reconstruction failed with error " << e.what());
-// }
+static void
+tempRecCb(int64_t time, uint32_t iter)
+{
+	if (info || verbose)
+		cout << "Reconstructed temporally in " << time << " ms with " << iter << " iterations"
+			 << "\n";
+}
+
+static void
+spatRecCb(int64_t time, uint32_t iter)
+{
+	if (info || verbose)
+		cout << "Reconstructed spatially in " << time << " ms with " << iter << " iterations"
+			 << "\n";
+}
+static void
+recErrorCb(const klab::KException &e)
+{
+	cout << "Reconstruction failed with error " << e.what();
+}
 
 int main(int argc, char *argv[])
 {
@@ -74,9 +96,9 @@ int main(int argc, char *argv[])
 			 m = DEFAULT_M,
 			 l = DEFAULT_L;
 	double channelDelayTmp = DEFAULT_CHANNELDELAY_MS;
-	bool verbose = false,
-		 info = false,
-		 seq = false;
+	bool seq = false,
+		 precode = true,
+		 bpSpat = false;
 	std::string matFilePath = DEFAULT_FILE,
 				// matFilePathOut = DEFAULT_FILEOUT,
 		srcMatrixName = DEFAULT_SRCMAT_NAME,
@@ -87,6 +109,8 @@ int main(int argc, char *argv[])
 	cmd.AddValue("info", "Enable info messages", info);
 	cmd.AddValue("verbose", "Verbose Mode", verbose);
 	cmd.AddValue("seq", "Reconstruct sequentially for each received packet", seq);
+	cmd.AddValue("precode", "Enable spatial precoding?", precode);
+	cmd.AddValue("bp", "Basis Pursuit when solving spatially?", bpSpat);
 	cmd.AddValue("nSrcNodes", "NOF source nodes in topology", nSrcNodes);
 	cmd.AddValue("dataRate", "data rate [mbps]", dataRate);
 	cmd.AddValue("channelDelay", "delay of all channels [ms]", channelDelayTmp);
@@ -96,7 +120,7 @@ int main(int argc, char *argv[])
 	cmd.AddValue("file", "path to mat file to read from", matFilePath);
 	// cmd.AddValue("file", "path to mat file to write output to", matFilePathOut);
 	cmd.AddValue("MATsrc", "name of the matrix in the mat file containing the data for the source nodes", srcMatrixName);
-	cmd.AddValue("MATk", "name of the varial in the mat file containing the value of k", kName);
+	cmd.AddValue("MATk", "name of the variable in the mat file containing the value of k", kName);
 
 	cmd.Parse(argc, argv);
 
@@ -137,7 +161,7 @@ int main(int argc, char *argv[])
 	NS_LOG_INFO("Reading mat file...");
 	matHandler_glob.Open(matFilePath);
 	DataStream<double> sourceData = matHandler_glob.ReadMat<double>(srcMatrixName);
-	// uint32_t k = matHandler_glob.ReadValue<double>(kName); // casting double  to uint32_t
+	uint32_t k = matHandler_glob.ReadValue<double>(kName); // casting double  to uint32_t
 	//matHandler_glob.Open(matFilePathOut);				   // open output file
 	/*********  initialize nodes  **********/
 
@@ -158,8 +182,12 @@ int main(int argc, char *argv[])
 	clusterHelper.SetSrcAppAttribute("ComprTemp", PointerValue(comprTemp));
 	clusterHelper.SetClusterAppAttribute("ComprTemp", PointerValue(comprTemp));
 
-	double txProb = TXPROB_MODIFIER * l / nSrcNodes;
-	clusterHelper.SetSrcAppAttribute("TxProb", DoubleValue(txProb));
+	if (precode)
+	{
+		double txProb = TXPROB_MODIFIER * l / nSrcNodes;
+		if (txProb <= 1)
+			clusterHelper.SetSrcAppAttribute("TxProb", DoubleValue(txProb));
+	}
 
 	Ptr<Compressor<double>> comp = CreateObject<Compressor<double>>();
 	comp->TraceConnectWithoutContext("Complete", MakeCallback(&compressCb));
@@ -218,26 +246,29 @@ int main(int argc, char *argv[])
 	// //recSpat->SetAttribute("k", UintegerValue(k));
 	// recSpat->TraceConnectWithoutContext("RecError", MakeCallback(&recErrorCb));
 	// sinkApp->SetAttribute("RecSpat", PointerValue(recSpat));
-	
+
 	Ptr<Reconstructor> rec = CreateObject<Reconstructor>();
-	Ptr<TransMatrix<double>>transMat = CreateObject<DcTransMatrix<double>>();
+	Ptr<TransMatrix<double>> transMat = CreateObject<DcTransMatrix<double>>();
 	Ptr<RandomMatrix> ranMat = CreateObject<IdentRandomMatrix>();
 	rec->SetAttribute("RecMatTemp", PointerValue(Create<RecMatrixReal>(ranMat, transMat)));
 	ranMat = CreateObject<GaussianRandomMatrix>();
 	rec->SetAttribute("RecMatSpat", PointerValue(Create<RecMatrixReal>(ranMat, transMat)));
 	sinkApp->SetAttribute("Reconst", PointerValue(rec));
- 
-    Config::Set("/NodeList/*/ApplicationList/*/$CsSinkApp/Reconst/AlgoSpat/$CsAlgorithm_OMP/k", UintegerValue(nSrcNodes+1));
+
+	if (bpSpat)
+		Config::Set("/NodeList/*/ApplicationList/*/$CsSinkApp/Reconst/AlgoSpat", PointerValue(CreateObject<CsAlgorithm_BP>()));
+
+	Config::Set("/NodeList/*/ApplicationList/*/$CsSinkApp/Reconst/AlgoTemp/$CsAlgorithm_OMP/k", UintegerValue(k));
+	Config::Set("/NodeList/*/ApplicationList/*/$CsSinkApp/Reconst/AlgoSpat/$CsAlgorithm_OMP/k", UintegerValue(k));
 	// recTemp->TraceConnectWithoutContext("RecError", MakeCallback(&recErrorCb));
-
-	// Ptr<Reconstructor<double>> recSpat = CreateObject<OMP_Reconstructor<double>>();
-	// recSpat->SetTransMat(transMat);
-	// //recSpat->SetAttribute("k", UintegerValue(k));
-
+	Config::ConnectWithoutContext("/NodeList/*/ApplicationList/*/$CsSinkApp/Reconst/AlgoSpat/$CsAlgorithm/RecComplete", MakeCallback(&spatRecCb));
+	Config::ConnectWithoutContext("/NodeList/*/ApplicationList/*/$CsSinkApp/Reconst/AlgoTemp/$CsAlgorithm/RecComplete", MakeCallback(&tempRecCb));
+	Config::ConnectWithoutContext("/NodeList/*/ApplicationList/*/$CsSinkApp/Reconst/AlgoTemp/$CsAlgorithm/RecError", MakeCallback(&recErrorCb));
+	Config::ConnectWithoutContext("/NodeList/*/ApplicationList/*/$CsSinkApp/Reconst/AlgoSpat/$CsAlgorithm_OMP/RecError", MakeCallback(&recErrorCb));
 	if (!seq)
 		sinkApp->SetAttribute("MinPackets", UintegerValue(l));
 	else
-		sinkApp->SetAttribute("MinPackets", UintegerValue(l/2));
+		sinkApp->SetAttribute("MinPackets", UintegerValue(0));
 
 	sinkApp->TraceConnectWithoutContext("Rx", MakeCallback(&receiveCb));
 	sinkApp->AddCluster(&cluster);
@@ -249,21 +280,16 @@ int main(int argc, char *argv[])
 	clusterApps.Start(Seconds(0.));
 	//	Simulator::Stop(Seconds(30));
 	Simulator::Run();
-
-	//write output data
-	// DataStreamContainer<double> dataOutCluster = cluster.GetStreams();
-	// matHandler_glob.WriteStruct<double>(cluster);
-	// // Ptr<CsNode> clusterNode = cluster.GetClusterNode();
-	// matHandler_glob.WriteStruct<double>(*clusterNode);
-	// for (auto it = cluster.SrcBegin(); it != cluster.SrcEnd(); it++)
-	// {
-	// 	matHandler_glob.WriteStruct<double>(**it);
-	// }
 	matHandler_glob.WriteCluster(cluster);
 	matHandler_glob.WriteValue<double>("nNodesUsed", nSrcNodes + 1);
 	matHandler_glob.WriteValue<double>("n", n);
 	matHandler_glob.WriteValue<double>("m", m);
 	matHandler_glob.WriteValue<double>("l", l);
+	if (!seq)
+		matHandler_glob.WriteValue<double>("attempts", 0);
+	else
+		matHandler_glob.WriteValue<double>("attempts", l);
+
 	Simulator::Destroy();
 	return 0;
 }
