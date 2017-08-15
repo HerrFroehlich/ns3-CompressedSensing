b657c83bb01740fd3f527b115e64670bd9322610
Tobias <twaurick@gmx.net>
2017-08-15 11:48:48 +0200

    New/Updated scripts to create data

---
+++ src_additional/Matlab/createData/createDCT2d.m
@@ -0,0 +1,90 @@
+%% SETTINGS
+FILE_PATH = './out/data';       %output mat file base name
+m = 64;                         %size of compressed measurement vector
+%n = 512;                        %size of measurement vector per sequence
+%% DEFINES
+MAX_NODES = 256;
+RAND_MEAN = 10;
+%% INPUT
+nNodes = input('Number of nodes: ');
+assert(nNodes > 0, 'Not enough nodes');
+assert(nNodes <= MAX_NODES, 'too many nodes');
+
+nSamp = input('NOF Samples: ');
+rho = input('Sparsity Ratio: ');
+assert(rho>=0 && rho<=1, 'Sparsity Ratio must be 0...1');
+
+nMeas = input('NOF measurement sequences [Default 1]:');
+if isempty(nMeas)
+    nMeas = 1;
+end
+assert(nMeas >=0, 'Must be greater/equal zero!');
+
+
+var_c = input('Power [Default 1]:');
+if isempty(var_c)
+    var_c = 1;
+end
+assert(var_c >=0, 'Must be greater/equal zero!');
+
+%% random dct coefficients
+
+X = zeros(nSamp*nMeas, nNodes);
+
+for i = 0:nMeas-1
+    k = round(rho * nSamp);
+
+    Xf = zeros(nSamp, nNodes);
+    idx = randperm(nSamp*nNodes, k);
+
+    Xf(idx) =  sqrt(var_c*nSamp*nNodes/k)*randn(k, 1) + RAND_MEAN;
+    Xi = idct2(Xf);
+    range = (i*nSamp)+1:(i+1)*nSamp;
+    X(range,:) = Xi;
+end
+%% write to file
+save(FILE_PATH, 'X', 'k', '-v6');
+
+fid = fopen([FILE_PATH 'INFO'], 'w');
+fprintf(fid, 'Number of nodes: %d\n', nNodes);
+fprintf(fid, 'Number of samples: %d\n', nSamp);
+fprintf(fid, 'Number of sequences: %d\n', nMeas);
+fprintf(fid, 'Sparsity k: %d\n', k);
+fprintf(fid, 'Power: %f\n', var_c);
+fclose(fid);
+
+
+%% Calculate Y to check
+
+Y = zeros(nNodes, m);
+A = randn(m,nSamp);
+idx = randperm(nSamp, m); % random subsampling
+for i=1:nNodes
+  %  A = randn(m,nSamp);
+  %   Y(i,:) = A*X(:,i);
+  % idx = randperm(nSamp, m); % random subsampling
+    idx = sort(idx);
+    x =  Xi(:,i);
+    Y(i,:) = x(idx);
+end
+
+%% informative plots
+close all
+
+% X DCT
+figure;
+f = repmat((0:nSamp-1)'/nSamp,1,nNodes);
+mesh(1:nNodes,f, abs(dct(Xi)));
+title ('DCT of X, last sequence')
+xlabel('Node');
+ylabel('F');
+zlabel('|A|');
+
+% Y DCT with random subsampling
+figure;
+f = repmat((0:nNodes-1)'/nNodes,1,m);
+mesh(1:m,f, abs(dct(Y)));
+title ('DCT of Y = A*X, last sequence')
+xlabel('Column');
+ylabel('F');
+zlabel('|A|');
--- src_additional/Matlab/createData/createDCT_JSM2.m
+++ src_additional/Matlab/createData/createDCT_JSM2.m
@@ -1,13 +1,10 @@
-% Create signals sparse in a DCT with a JSM1 model.
-% The DCT coefficients are chosen from a normal distribution.
-% 
-% Author : Tobias Waurick
-% Date   : 09.08.17
 
 %% SETTINGS
 FILE_PATH = './out/data';       %output mat file base name
 m = 64;                         %size of compressed measurement vector
 %n = 512;                        %size of measurement vector per sequence
+
+                      %size of measurement vector per sequence
 %% DEFINES
 MAX_NODES = 256;
 
@@ -20,22 +17,14 @@ nSamp = input('NOF Samples: ');
 rho_c = input('Common Sparsity Ratio: ');
 assert(rho_c>=0 && rho_c<=1, 'Sparsity Ratio must be 0...1');
 
-rho = input('Innovative Sparsity Ratio: ');
-assert(rho>=0 && rho<=1, 'Sparsity ratio must be 0...1');
-
-var_c = input('Power Ratio Common/Innovative [Default 1]:');
-if isempty(var_c)
-    var_c = 1;
-end
-assert(var_c >=0, 'Must be greater/equal zero!');
-
 fc = input('Maximum discrete frequency of common part [Default 1]:');
 if isempty(fc)
     fc = 1;
 end
 assert(fc>=0 && fc<=1, 'Frequency must be 0...1');
 rng('shuffle'); %set seed based on current time
-%% Create common DCT sparse signals
+
+%%
 
 k_c = round(rho_c * nSamp);
 fIdx = round(fc*nSamp);
@@ -43,27 +32,11 @@ if fIdx < k_c
     k_c = fIdx;
 end
 
-%xf = sqrt(var_c)*randn(nSamp,1);
-%Xf = repmat(xf,1, nNodes);
-Xfsparse = repmat(sqrt(var_c)*randn(k_c,1), 1, nNodes);
-Xf = zeros(nSamp, nNodes);
 idx = randperm(fIdx, k_c);
-%Xf(idx,:) = 0;
-Xf(idx,:) = Xfsparse;
-Xc = idct(Xf);
 
-%% Create innovative sparse signals
-k = round(rho * nSamp);
-Xf = randn(nSamp,nNodes);
-for i = 1:nNodes
-    idx = randperm(nSamp, nSamp-k);
-    Xf(idx,i) = 0;
-end
-
-Xi = idct(Xf);
-
-%% join them
-X = Xi + Xc;
+Xf = zeros(nSamp, nNodes);
+Xf(idx,:) = randn(k_c, nNodes);
+X = idct(Xf);
 
 %% write to file
 save(FILE_PATH, 'X', 'k', '-v6');
@@ -97,22 +70,10 @@ close all
 % Xc DCT
 figure;
 f = repmat((0:nSamp-1)'/nSamp,1,nNodes);
-mesh(1:nNodes,f, abs(dct(Xc)));
+mesh(1:nNodes,f, abs(dct(X)));
 title ('DCT of common(X)')
 xlabel('Node');
 ylabel('F');
-
-
-zlabel('|A|');
-
-% Xi DCT
-figure;
-f = repmat((0:nSamp-1)'/nSamp,1,nNodes);
-mesh(1:nNodes,f, abs(dct(Xi)));
-title ('DCT of innovative(X)')
-xlabel('Node');
-ylabel('F');
-zlabel('|A|');
 
 % X DCT
 figure;
@@ -131,4 +92,3 @@ title ('DCT of Y = A*X')
 xlabel('Column');
 ylabel('F');
 zlabel('|A|');
-
